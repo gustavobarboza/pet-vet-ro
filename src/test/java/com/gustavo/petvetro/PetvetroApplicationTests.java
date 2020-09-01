9f077c866f4af48fd38d383eba2b8d9e33781e2e
Gustavo Barboza <gustavobarboza.oliveira@gmail.com>
2020-09-01 20:32:52 -0300

    Projeto inicial

---
+++ src/test/java/com/gustavo/petvetro/PetvetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavo.petvetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetvetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
